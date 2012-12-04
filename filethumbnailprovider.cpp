82f2c4891b091ea2ab7752ecda6026a882b2627e
Coley <coley@20c9d04670c1.ant.amazon.com>
2012-12-04 09:56:48 -0800

    Starting to get the admin mode

--- filethumbnailprovider.cpp
+++ filethumbnailprovider.cpp
@@ -1,5 +1,7 @@
 #include "filethumbnailprovider.h"
 
+#include "myicon.h"
+
 #include <QDebug>
 
 #define EnableThumbnail 1
@@ -10,23 +12,23 @@ QFileThumbnailProvider::QFileThumbnailProvider()
 
 QIcon   QFileThumbnailProvider::icon(IconType type) const
 {
-    //qDebug() << "icon(IconType type)" << type;
+    //qDebug() << __FILE__ << __FUNCTION__ << "icon(IconType type)" << type;
     return QFileIconProvider::icon(type);
 }
 
 QIcon   QFileThumbnailProvider::icon(const QFileInfo &info) const
 {
+    qDebug() << __FILE__ << __FUNCTION__ << "icon(const QFileInfo &info)" << info.fileName();
 #if EnableThumbnail
-    QIcon icon(info.absoluteFilePath());
+    QMyIcon icon(info.absoluteFilePath());
     return icon;
 #else
-    //qDebug() << "icon(const QFileInfo &info)" << info.fileName();
     return QFileIconProvider::icon(info);
 #endif
 }
 
 QString QFileThumbnailProvider::type(const QFileInfo &info) const
 {
-    //qDebug() << "type(const QFileInfo &info)" << info.fileName();
+    //qDebug() << __FILE__ << __FUNCTION__ << "type(const QFileInfo &info)" << info.fileName();
     return QFileIconProvider::type(info);
 }
